24de222945b16e1d52ba2569fbddbd68b8152b6c
Matt Clarke <mjclarke94@gmail.com>
2021-05-04 11:30:11 +0100

    Bump to py3.9

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import find_packages, setup
 setup(
     name="LaTeX oriented pre-commit hooks",
     description="Contains hook(s) for pre-commit, see http://pre-commit.com",
-    author="Jonas Bushart <jonas@bushart.org>",
-    version="1.1.0",
+    author="Jonas Bushart <mjclarke94@gmail.com>",
+    version="1.2.0",
     classifiers=[
         "License :: OSI Approved :: Apache 2 License",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.9",
     ],
     packages=find_packages(),
     entry_points={
