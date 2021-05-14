ee94db193631624bf2ac2ec2cbcd1763858a5f08
Matt Clarke <mjclarke94@gmail.com>
2021-05-14 14:50:31 +0100

    Add field stripping

--- latexhooks/sort_bib.py
+++ latexhooks/sort_bib.py
@@ -4,13 +4,15 @@ import os
 from pybtex.database import OrderedCaseInsensitiveDict, parse_file
 
 
-def sort_library(library):
+def sort_library(library, banned_fields: set = {'abstract', 'file', 'keywords', 'mendeley-tags'}):
     original_keys = list(library.entries.keys())
     library.original_keys = original_keys
 
     sorted_entries = sorted(library.entries.items(), key=lambda x: x[1].key)
+    trimmed_entries = [(k, drop_fields(v, banned_fields)) for k, v in sorted_entries]
+
     library.entries = OrderedCaseInsensitiveDict()
-    library.add_entries(sorted_entries)
+    library.add_entries(trimmed_entries)
 
     library.sorted_keys = list(library.entries.keys())
 
@@ -19,6 +21,12 @@ def check_reorder(library):
     if library.original_keys == library.sorted_keys:
         return False
     return True
+
+def drop_fields(entry, banned_fields: set = {}):
+    fields = set(entry.fields.keys())
+    for k in fields.intersection(banned_fields):
+        del entry.fields[k]
+    return entry
 
 
 def overwrite_library(library, filename):
@@ -29,6 +37,7 @@ def main(argv=None):
 
     parser = argparse.ArgumentParser()
     parser.add_argument("filenames", nargs="*", help="Filenames to run")
+    parser.add_argument("--banned-fields", nargs="*", help="Fields to strip from bibitem entries", dest='banned', default=['abstract', 'file', 'keywords', 'mendeley-tags'])
     parser.add_argument(
         "--silent-overwrite", action="store_true", dest="silent", default=False
     )
@@ -39,10 +48,12 @@ def main(argv=None):
 
     return_value = 0
 
+    banned_fields = set(args.banned)
+
     for filename in args.filenames:
 
         library = parse_file(filename)
-        sort_library(library)
+        sort_library(library, banned_fields=banned_fields)
 
         if check_reorder(library):
             if args.check_only:
